99ff6b098e511347a96af8df432bea3f18bb6764
raviteja93812 <108179412+raviteja93812@users.noreply.github.com>
2022-07-04 20:51:33 +0530

    Add files via upload

---
+++ Backend/test/java/com/wipro/hrms/Emart.java
@@ -0,0 +1,13 @@
+package com.wipro.hrms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheSuperStoreTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
